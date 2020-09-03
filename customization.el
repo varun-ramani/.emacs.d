219f3e19b02904c871a43adc8766d63e2d98ac7c
Varun Ramani <varun.ramani@gmail.com>
2020-09-03 19:02:59 -0400

    Moved customizations to customization.el

---
+++ customization.el
@@ -0,0 +1,16 @@
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq default-directory "~")
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+
+(setq org-agenda-timegrid-use-ampm t)
+
+(custom-set-variables
+ ;; Custom variables go here
+ )
+(custom-set-faces
+ ;; And Custom faces go here
+ )
+
--- init.el
+++ init.el
@@ -1,12 +1,6 @@
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq default-directory "~")
-
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-
 (electric-pair-mode)
 
 (load "~/.emacs.d/core/packages.el")
@@ -22,18 +16,3 @@
 (load "~/.emacs.d/appearance.el")
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default))
- '(org-agenda-files '("~/Dropbox/org/MATH141.org" "~/Dropbox/org/BSCI189I.org"))
- '(package-selected-packages '(evil-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

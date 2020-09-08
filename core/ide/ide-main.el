26a89cda4e18c36ffe26d3639da19f6f094ad49b
Varun Ramani <varun.ramani@gmail.com>
2020-09-08 09:51:12 -0400

    Started adding debugging support

--- core/ide/ide-main.el
+++ core/ide/ide-main.el
@@ -8,4 +8,16 @@
   (setq read-process-output-max (* 1024 1024))
   (setq lsp-idle-delay 0.400))
 
+(use-package dap-mode
+  :ensure t)
+
+(use-package lsp-ui
+  :ensure t)
+
+(use-package flycheck
+  :ensure t)
+
+(setq lsp-prefer-flymake nil)
+(setq lsp-ui-sideline-enable nil)
+
 (global-company-mode)

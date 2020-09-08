bbff3ec1af7cd4ce41b7107fbe256bae16050b1e
Varun Ramani <varun.ramani@gmail.com>
2020-09-08 09:51:47 -0400

    Treemacs, doom-modeline

--- core/interface.el
+++ core/interface.el
@@ -8,7 +8,26 @@
   :config
   (counsel-mode))
 
+(use-package treemacs
+  :ensure t)
+
+(use-package treemacs-projectile
+  :ensure t)
+
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package xterm-color
+  :ensure t)
+(setq compilation-environment '("TERM=xterm-256color"))
+
+(defun my/advice-compilation-filter (f proc string)
+  (funcall f proc (xterm-color-filter string)))
+
+(advice-add 'compilation-filter :around #'my/advice-compilation-filter)
